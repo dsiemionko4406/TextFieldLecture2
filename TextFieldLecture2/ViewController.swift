15a1d50c5baa78545a0fed052271d825e0f48b97
David Siemionko <dsiemionko4406@stu.d214.org>
2020-10-28 15:41:40 -0400

    Initial Commit

---
+++ TextFieldLecture2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldLecture2
+//
+//  Created by user180860 on 10/28/20.
+//  Copyright © 2020 user180860. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
